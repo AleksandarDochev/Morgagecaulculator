f60cab18709eee08b1b02e43b301c181f2662a94
Alex <aleksandaredochev@gmail.com>
2021-02-10 21:21:21 +0200

    All user input data framework and confirmation return print

--- src/com/MorgagePackedge/Main.java
+++ src/com/MorgagePackedge/Main.java
@@ -6,13 +6,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner UserPrincipalInput = new Scanner(System.in);
-        //Creating the input it self named UserPrincipalInput
+        Scanner UserInputPrincipal = new Scanner(System.in);
+        //Creating the input it self named UserInputPrincipal
         //System.in is for the program to know that the input is
         // coming from the terminal
         System.out.print("Enter principal: ");
-        int principal = UserPrincipalInput.nextInt();
-        System.out.println(principal);
-//
+        int principal = UserInputPrincipal.nextInt();
+        //principal input and memorisation /\/\
+
+        Scanner UserInputAnnualInterestRate = new Scanner(System.in);
+        System.out.print("Enter Annual Interest Rate: ");
+        float AnnualInterestRate = UserInputAnnualInterestRate.nextFloat();
+        //Annual Interest Rate input and memorisation /\/\
+
+        System.out.print("Enter your Period in Years: ");
+        Scanner UserInputPeriodYears = new Scanner(System.in);
+        int PeriodYears = UserInputPeriodYears.nextInt();
+        //Period in Years input and memorisation /\/\
+
+        System.out.println("Your principal: "+ principal);
+        System.out.println("Your Annual Interest Rate: "+ AnnualInterestRate);
+        System.out.println("Your Period (Years): " + PeriodYears);
+        //System print out memoraised values for testing and confirmation /\/\
+
+
     }
 }
