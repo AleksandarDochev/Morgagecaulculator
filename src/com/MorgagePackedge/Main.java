3ff3a5ccc98e7bff0c85d5d671a3f45b2d542301
Alex <aleksandaredochev@gmail.com>
2021-02-10 20:57:35 +0200

    Basic user input test part1

--- src/com/MorgagePackedge/Main.java
+++ src/com/MorgagePackedge/Main.java
@@ -1,8 +1,18 @@
 package com.MorgagePackedge;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner UserPrincipalInput = new Scanner(System.in);
+        //Creating the input it self named UserPrincipalInput
+        //System.in is for the program to know that the input is
+        // coming from the terminal
+        System.out.print("Enter principal: ");
+        int principal = UserPrincipalInput.nextInt();
+        System.out.println(principal);
 //
     }
 }
